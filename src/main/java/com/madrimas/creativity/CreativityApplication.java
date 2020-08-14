9ec3254c86508dc8f8027d21c877a408edaf792e
madrimas <zapiorkowski@gmail.com>
2020-08-14 23:39:23 +0200

    persistence.xml created

--- src/main/java/com/madrimas/creativity/CreativityApplication.java
+++ src/main/java/com/madrimas/creativity/CreativityApplication.java
@@ -3,11 +3,22 @@ package com.madrimas.creativity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @SpringBootApplication
 public class CreativityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreativityApplication.class, args);
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("creativity");
+//		EntityManager entityManager = entityManagerFactory.createEntityManager();
+//
+//		entityManager.close();
+//		entityManagerFactory.close();
+//
+//		System.out.println("Done!");
 	}
 
 }
